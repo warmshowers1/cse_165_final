adcce5d4a35c789490592d92049e6334ba8dd0ae
Alex Woersching <ajw006@gmail.com>
2020-12-09 22:05:31 -0800

    Eliminates default values from MegaAlien

--- MegaAlien.cpp
+++ MegaAlien.cpp
@@ -1,6 +1,6 @@
 #include "MegaAlien.h"
 #include <iostream>
 
-MegaAlien::MegaAlien(float x=0, float y=-0.9, float w=0.1, float h=0.1): Alien("images/alien1.png", x, y, w, h){
+MegaAlien::MegaAlien(float x, float y, float w, float h): Alien("images/alien1.png", x, y, w, h){
     lives = 2;
 }
